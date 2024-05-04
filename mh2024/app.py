d5c3acb038015ccc7bc9d9a4b7cb666970487bf4
Seb18456 <sebastian18678@gmail.com>
2024-05-04 17:19:56 -0400

    Got Flask Working

--- mh2024/app.py
+++ mh2024/app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__)
+    @app.route("/")
+    def home():
+        return render_template("home.html")
 
     return app
 
