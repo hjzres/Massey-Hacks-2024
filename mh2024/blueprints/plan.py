936f8beccc81838e7d84ff610cda409c6ab6492c
Andrei <142443853+hjzres@users.noreply.github.com>
2024-05-04 20:31:35 -0400

    made a html for workout create and workout search, and made a part in the blueprint for each stuff

--- mh2024/blueprints/plan.py
+++ mh2024/blueprints/plan.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 from mh2024 import crud
 
@@ -6,4 +6,12 @@ bp = Blueprint("user", __name__, url_prefix="/user")
 
 bp.route("/workout")
 def workout():
-    pass
+    return(render_template("workoutplans.html"))
+
+bp.route("/workout/create")
+def workout_create():
+    return(render_template("workout-create.html"))
+
+bp.route("/workout/search")
+def workout_search():
+    return(render_template("workout-search.html"))
