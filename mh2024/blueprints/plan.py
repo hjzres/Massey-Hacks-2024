a783f930bc887503a75a2d4545b6374b9b9457b0
hjzre <142443853+hjzres@users.noreply.github.com>
2024-05-05 04:40:22 -0400

    adding works now

--- mh2024/blueprints/plan.py
+++ mh2024/blueprints/plan.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 from mh2024 import crud
 from mh2024 import data
@@ -10,10 +10,14 @@ def workout(username):
     user = crud.get_user_by_username(username)
     return(render_template("workoutplans.html", user=user))
 
-@bp.route("/workout/<day>/<username>")
+@bp.route("/workout/<day>/<username>", methods=["GET", "POST"])
 def day(day, username):
     user = crud.get_user_by_username(username)
     workouts = data.get_day_info(user.username, day)
+    if request.method == "POST":
+        workout = request.form["workout"]
+        sets = [request.form["set1"], request.form["set2"], request.form["set3"]]
+        data.add_workout(user.username, day, workout, sets)
     return(render_template("day.html", user=user, day=day, workouts=workouts))
 
 @bp.route("/workout/search")
