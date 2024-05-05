14fedcdd42299ec0329dee28b2be22c3ae600e80
hjzre <142443853+hjzres@users.noreply.github.com>
2024-05-05 02:36:26 -0400

    Made a function in data.py that adds a user (Not Tested)

--- mh2024/data.py
+++ mh2024/data.py
@@ -1,9 +1,23 @@
 import json
 
-def load_file(file):
-    with open(f"./data/{file}.json", "r") as f:
+def add_user(user:str):
+    cache:dict = load_file()
+    cache[user] = {
+        "Monday":{},
+        "Tuesday":{},
+        "Wednesday":{},
+        "Thursday":{},
+        "Friday":{},
+        "Saturday":{},
+        "Sunday":{}
+    }
+    post_file(cache)
+
+
+def load_file():
+    with open(f"./data/cache.json", "r") as f:
         return json.load(f)
 
-def post_file(data, file):
-    with open(f"./data/{file}.json", "w") as f:
+def post_file(data):
+    with open(f"./data/cache.json", "w") as f:
         json.dump(data, f, indent=4)
